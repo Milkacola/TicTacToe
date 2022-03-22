f6fd6951c0bb1ed7a390e92a701b46034206f6d0
Kovács Milán <95681544+Milkacola@users.noreply.github.com>
2022-03-22 12:10:24 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", init);
+
+function ID(elem) {
+    return document.getElementById(elem);
+}
+function CLASS(elem) {
+    return document.getElementsByClassName(elem);
+}
+function $(elem) {
+    return document.querySelectorAll(elem);
+}
+
+var jatek = [];
+
+function init() {
+    jatekter();
+}
+
+function jatekter() {
+    var txt="";
+    for (let index = 0; index < 9; index++) {
+        txt += `<div class="mezo"><p>div${[index+1]}</p>
+        </div>`
+
+        
+    }
+    $(".container")[0].innerHTML=txt;
+    console.log(txt);
+}
+
+
